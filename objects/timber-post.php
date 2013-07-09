6ee3e3177bd2190d618c5f34e90a1416f2b1d911
jarednova <jarednova@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-09 21:00:32 +0000

    merged Timber 0.10
    git-svn-id: https://plugins.svn.wordpress.org/timber-library/trunk@738462 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ objects/timber-post.php
@@ -0,0 +1,373 @@
+<?php
+
+	class TimberPost extends TimberCore {
+
+		var $ImageClass = 'TimberImage';
+		var $PostClass = 'TimberPost';
+
+		var $_can_edit;
+
+		/**
+		*	If you send the contructor nothing it will try to figure out the current post id based on being inside The_Loop
+		*	@param mixed $pid	
+		*	@return a TimberPost object -- woo!
+		*/
+		function __construct($pid = null){
+			if ($pid === null && have_posts()){
+				ob_start();
+				the_post();
+				$pid = get_the_ID();
+				$this->ID = $pid;
+				ob_end_clean();
+			}
+			if (is_numeric($pid)){
+				$this->ID = $pid;
+			}
+			$this->init($pid);
+			return $this;
+		}
+
+		function init($pid = false){
+			if ($pid === false){
+				$pid = get_the_ID();
+			}
+			$this->import_info($pid);
+		}
+
+ 		function get_edit_url(){
+ 			if ($this->can_edit()){
+ 				return '/wp-admin/post.php?post='.$this->ID.'&action=edit';
+ 			} 
+ 			return false;
+ 		}
+
+		/**
+		*	updates the post_meta of the current object with the given value
+		*
+		*	@param string $field
+		*	@param mixed $value
+		*/
+		function update($field, $value){
+			if (isset($this->ID)) {
+				update_post_meta($this->ID, $field, $value);
+				$this->$field = $value;
+			} 
+		}
+
+
+		/**
+		*	takes a mix of integer (post ID), string (post slug), or object to return a WordPress post object from WP's built-in get_post() function
+		*
+		*	@param mixed $pid
+		*	@return WP_Post on success
+		*/
+		private function prepare_post_info($pid = 0){
+			if (is_string($pid) || is_numeric($pid) || (is_object($pid) && !isset($pid->post_title)) || $pid === 0){
+				$pid = self::check_post_id($pid);
+				$post = get_post($pid);
+				if ($post){
+					return $post;
+				} else {
+					$post = get_page($pid);
+					return $post;
+				}
+			} 
+			return $pid;
+		}
+
+
+		/**
+		*	helps you find the post id regardless of whetehr you send a string or whatever
+		*	
+		*	@param mixed $pid;
+		*	@return integer ID number of a post
+		*/
+		private function check_post_id($pid){
+			if (is_numeric($pid) && $pid === 0){
+				$pid = get_the_ID();
+				return $pid;
+			}
+			if (!is_numeric($pid) && is_string($pid)){
+				$pid = self::get_post_id_by_name($pid);
+				return $pid;
+			}
+			if (!$pid){
+				return;
+			}
+			return $pid;
+		}
+
+
+		/** 
+		*	get_post_id_by_name($post_name)
+		*
+		*/
+		function get_post_id_by_name($post_name){
+			global $wpdb;
+			$query = "SELECT ID FROM $wpdb->posts WHERE post_name = '$post_name'";
+			$result = $wpdb->get_row($query);
+			return $result->ID;
+		}
+
+		function get_preview($len = 50, $force = false, $readmore = 'Read More', $strip = true){
+			$text = '';
+			$trimmed = false;
+			if (isset($this->post_excerpt) && strlen($this->post_excerpt)){
+				if ($force){
+					$text = WPHelper::trim_words($this->post_excerpt, $len);
+					$trimmed = true;
+				} else {
+					$text = $this->post_excerpt;
+				}
+			}
+			if (!strlen($text)){
+				$text = WPHelper::trim_words($this->get_content(), $len, false);
+				$trimmed = true;
+			}
+			if (!strlen(trim($text))){
+				return $text;
+			}
+			if ($strip){
+				$text = trim(strip_tags($text));
+			}
+			if (strlen($text)){
+				$text = trim($text);
+				$last = $text[strlen($text)-1];
+				if ($last != '.' && $trimmed){
+					$text .= ' &hellip; ';
+				}
+				$text .= ' <a href="'.$this->get_path().'" class="read-more">'.$readmore.'</a>';
+			}
+			return $text;
+		}
+
+		/** 
+		*	gets the post custom and attaches it to the current object
+		*	@param integer $pid a post ID number
+		*/
+		function import_custom($pid){
+			$customs = get_post_custom($pid);
+			foreach($customs as $key => $value){
+				$v = $value[0];
+				$this->$key = $v;
+				if (is_serialized($v)){
+					if (gettype(unserialize($v)) == 'array'){
+						$this->$key = unserialize($v);
+					}
+				}
+			}
+		}
+
+		function get_thumbnail(){
+			if (function_exists('get_post_thumbnail_id')){
+				$tid = get_post_thumbnail_id($this->ID);
+				if ($tid){
+					return new $this->ImageClass($tid);
+				}
+			}
+			return null;
+		}
+
+		function get_permalink(){
+			return get_permalink( $this->ID );
+		}
+
+		function get_link(){
+			if (isset($this->path)){
+				return $this->path;
+			}
+		}
+
+		function import_info($pid){
+			$post_info = $this->get_info($pid);
+			$this->import($post_info);			
+		}
+
+		function get_parent(){
+			if (!$this->post_parent){
+				return false;
+			}
+			return new $this->PostClass($this->post_parent);
+		}
+
+		function get_author(){
+			if (isset($this->post_author)){
+				return new TimberUser($this->post_author); 
+			}
+			return false;
+		}
+
+		function get_info($pid){
+			global $wp_rewrite;
+			if (is_array($pid)){
+				//print_r(debug_backtrace());
+			}
+			$post = $this->prepare_post_info($pid);
+			
+			if (!isset($post->post_title)){
+				return;
+			}
+			$post->title = $post->post_title;
+			$post->slug = $post->post_name;
+			$post->display_date = date(get_option('date_format'), strtotime($post->post_date));
+
+			$this->import_custom($post->ID);
+			
+			if (isset($post->post_author)){
+				$post->author = new TimberUser($post->post_author); 
+			}
+			
+			$post->status = $post->post_status;	
+			if (!isset($wp_rewrite)){
+				return $post;
+			} else {
+				$post->permalink = get_permalink($post->ID);
+				$post->path = $this->url_to_path($post->permalink);
+			}
+			return $post;
+		}
+
+		function get_display_date($use = 'post_date'){
+			return date(get_option('date_format'), strtotime($this->$use));
+		}
+
+		function get_children($post_type = 'any', $childPostClass = false){
+			if ($childPostClass == false){
+				$childPostClass = $this->PostClass;
+			}
+			if (isset($this->children)){
+				return $this->children;
+			}
+			if ($post_type == 'parent'){
+				$post_type = $this->post_type;
+			}
+			$this->children = get_children('post_parent='.$this->ID.'&post_type='.$post_type);
+			foreach($this->children as &$child){
+				$child = new $childPostClass($child->ID);
+			}
+			return $this->children;
+		}
+
+		function get_comments($ct = 0, $type = 'comment', $status = 'approve', $CommentClass = 'TimberComment'){
+			$args = array('post_id' => $this->ID, 'status' => $status);
+			if ($ct > 0){
+				$args['number'] = $ct;
+			}
+			$comments = get_comments($args);
+			foreach($comments as &$comment){
+				$comment = new $CommentClass($comment);
+			}
+			return $comments;
+		}
+
+		function get_categories(){
+			return $this->get_terms('category');
+		}
+
+		function get_category(){
+			$cats = $this->get_categories();
+			if (count($cats) && isset($cats[0])){
+				return $cats[0];
+			}
+			return null;
+		}
+
+		function get_terms($tax = '', $merge = true){
+			if (!strlen($tax) || $tax == 'all' || $tax == 'any'){
+				$taxs = get_object_taxonomies($this->post_type);
+			} else {
+				$taxs = array($tax);
+			}
+			$ret = array();
+			foreach($taxs as $tax){
+				if ($tax == 'tags' || $tax == 'tag'){
+					$tax = 'post_tag';
+				} else if ($tax == 'categories'){
+					$tax = 'category';
+				}
+				$terms = wp_get_post_terms($this->ID, $tax);
+				foreach($terms as &$term){
+					$term = new TimberTerm($term->term_id);
+				}
+				if ($merge){
+					$ret = array_merge($ret, $terms);
+				} else if (count($terms)){
+					$ret[$tax] = $terms;
+				}
+			}
+			return $ret;
+		}
+
+		function get_image($field){
+			error_log('field='.$this->$field);
+			return new $ImageClass($this->$field);
+		}
+
+		function get_tags(){
+			$tags = get_the_tags($this->ID);
+			if (is_array($tags)){
+				$tags = array_values($tags);
+			} else {
+				$tags = array();
+			}
+			return $tags;
+		}
+
+		function get_title(){
+			$title = $this->post_title;
+			return apply_filters('the_title', $title);
+		}
+
+		function get_content($len = 0, $page = 0){
+			$content = $this->post_content;
+			if ($len){
+				wp_trim_words($content, $len);
+			}
+			if ($page){
+				$contents = explode('<!--nextpage-->', $content);
+				$page--;
+				if (count($contents) > $page){
+					$content = $contents[$page];
+				}
+			}
+			return apply_filters('the_content', ($content));
+		}
+
+		function get_post_type(){
+			return get_post_type_object($this->post_type);
+		}
+
+		function get_comment_count(){
+			if (isset($this->ID)){
+				return get_comments_number($this->ID);
+			} else {
+				return 0;
+			}
+		}
+
+		//This is for integration with Elliot Condon's wonderful ACF
+		function get_field($field_name){
+			return get_field($field_name, $this->ID);
+		}
+
+		function import_field($field_name){
+			$this->$field_name = $this->get_field($field_name);
+		}
+
+		//Deprecated
+		function children(){
+			return $this->get_children();
+		}
+
+		function terms($tax = ''){
+			return $this->get_terms($tax);
+		}
+
+		function get_path(){
+			return $this->get_link();
+		}
+
+		function tags(){
+			return $this->get_tags();
+		}
+	}
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== Timber ===
+Contributors: jarednova
+Tags: template engine, templates, twig
+Requires at least: 3.5
+Stable tag: trunk
+Tested up to: 3.5.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Helps you create themes faster with more sustainable code. With Timber, you write your HTML using Twig Templates separate from PHP. 
+
+== Description ==
+Timber cleans-up your theme code so, for example, your php file can focus on being the data, while your twig/html file can focus 100% on the HTML and display.
+
+Once Timber is installed and activated in your plugin directory, it gives any WordPress theme the ability to take advantage of the power of Twig and other Timber features.
+
+### Looking for docs?
+* [Timber Documentation](https://github.com/jarednova/timber/wiki/)
+* [Twig Reference](http://twig.sensiolabs.org/documentation)
+* **[Video Tutorials](https://github.com/jarednova/timber/wiki/Video-Tutorials)**
+* [Overview / Getting Started Guide](https://github.com/jarednova/timber/wiki/getting-started)
+
+#### What does it look like?
+Nothing. Timber is meant for you to build a theme on. Like the [Starkers](https://github.com/viewportindustries/starkers) or [Boilerplate theme](https://github.com/zencoder/html5-boilerplate-for-wordpress) it comes style-free, because you're the style expert. Instead, Timber handles the logic you need to make a kick-ass looking site.
+
+#### Who is it good for?
+Timber is great for any WordPress developer who cares about writing good, maintainable code. It helps teams of designers and developers working together. At [Upstatement](http://upstatement.com) we made Timber because not everyone knows the ins-and-outs of the_loop(), WordPress codex and PHP (nor should they). With Timber your best WordPress dev can focus on building the .php files with requests from WordPress and pass the data into .twig files. Once there, designers can easily mark-up data and build out a site's look-and-feel.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Activate the plugin through the 'Plugins' menu in WordPress
+2. For an example, try modifying your home.php or index.php with something like this:
+
+```php
+$context = array();
+$context['message'] = 'Hello Timber!';
+Timber::render('welcome.twig', $context);
+```
+
+Then create a subdirectory called `views` in your theme folder. The make this file: `views/welcome.twig`
+```html
+{# welcome.twig #}
+<div class="welcome">
+	<h3>{{message}}</h3>
+</div>
+```
+
+That's Timber!
+
+
+== Frequently Asked Questions ==
+
+= Can it be used in an existing theme? =
+
+You bet! Watch these **[video tutorials](https://github.com/jarednova/timber/wiki/Video-Tutorials)** to see how.
