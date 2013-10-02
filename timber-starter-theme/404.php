b1c7de2c2e3d1efdf455725a9d83c01fafd675ed
jarednova <jarednova@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-02 16:23:41 +0000

    Updating to 0.13.5
    git-svn-id: https://plugins.svn.wordpress.org/timber-library/tags/0.13.5@781335 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ timber-starter-theme/404.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * Methods for WPHelper can be found in the /functions sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+Timber::render('404.twig');
