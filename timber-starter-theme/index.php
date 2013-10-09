f7714bc502d1a3146fa9245a2f9038046e96091d
jarednova <jarednova@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-09 15:04:45 +0000

    Updating to 0.14
    git-svn-id: https://plugins.svn.wordpress.org/timber-library/trunk@785349 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- timber-starter-theme/index.php
+++ timber-starter-theme/index.php
@@ -4,9 +4,9 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * E.g., it puts together the home page when no home.php file
  *
- * Methods for WPHelper can be found in the /functions sub-directory
+ * Methods for TimberHelper can be found in the /functions sub-directory
  *
  * @package 	WordPress
  * @subpackage 	Timber
@@ -16,10 +16,16 @@
 	if (!class_exists('Timber')){
 		echo 'Timber not activated';
 	}
-	
+
 	$data = Timber::get_context();
+	$data['menu'] = new TimberMenu();
 	$posts = Timber::get_posts('TimberPost');
 	$data['posts'] = $posts;
+	$data['foo'] = 'bar';
+	$templates = array('index.twig');
+	if (is_home()){
+		array_unshift($templates, 'home.twig');
+	}
 	Timber::render('index.twig', $data);
 
 
